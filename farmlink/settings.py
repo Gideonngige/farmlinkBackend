48a5e0626d32ae35330e7efdd5ae0142e141373f
Gideonngige <ushindigideon01@gmail.com>
2025-06-03 23:52:23 +0300

    added frameworks

--- farmlink/settings.py
+++ farmlink/settings.py
@@ -37,9 +37,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'farmlinkApp',  # Your application
+    'corsheaders',
+    'rest_framework',  # Django REST Framework
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8081',
+    'http://localhost:8000',
+    'https://farmlinkbackend-qupt.onrender.com',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
