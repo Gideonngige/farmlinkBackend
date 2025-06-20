48edaaae9596c6900d2b5567d182e26890b56c6e
Gideonngige <ushindigideon01@gmail.com>
2025-06-20 11:57:32 +0300

    removed model

--- farmlinkApp/urls.py
+++ farmlinkApp/urls.py
@@ -13,5 +13,4 @@ urlpatterns = [
     path('get_products/<str:product_name>/', views.get_products, name='get_products'),
     path('buy/', views.buy, name='buy'),
     path('get_farmer_notifications/<int:farmer_id>/', views.get_farmer_notifications, name='get_farmer_notifications'),
-    path('ask_question/<str:question>/', views.ask_question, name='ask_question'),
 ]
